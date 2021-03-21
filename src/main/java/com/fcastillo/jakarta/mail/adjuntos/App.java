7da1927eee1451ccb78c6c278154e1a60426bc13
Francisco Castillo <cesarfranciscocastillo@gmail.com>
2021-03-21 19:54:06 -0300

    feat envio con archivos adjuntos

---
+++ src/main/java/com/fcastillo/jakarta/mail/adjuntos/App.java
@@ -0,0 +1,47 @@
+package com.fcastillo.jakarta.mail.adjuntos;
+
+import com.fcastillo.jakarta.mail.textoplano.*;
+import java.time.LocalDate;
+import java.util.Properties;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App {
+
+    private static final Logger LOG = LogManager.getLogger(App.class);
+
+    public static void main(String[] args) {
+
+        Properties properties = new Properties();
+
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.user", "remitente@gmail.com");
+        properties.put("mail.smtp.password", "password");
+
+        String remitente = "remitente@gmail.com";
+        String destinatario = "destinatario@gmail.com";
+        String asunto = "Archivos adjuntos";
+
+        StringBuilder mensaje = new StringBuilder();
+
+        mensaje.append("Hola\n\n")
+                .append("este es un mensaje de prueba en el que se envian archivos adjuntos, enviado el: ")
+                .append(LocalDate.now());
+
+        String[] files = new String[2];
+
+        files[0] = "/home/imagenes/imagen1.jpg";
+        files[1] = "/home/imagenes/imagen2.jpg";
+
+        boolean enviado = EmailManager.enviarAdjuntos(files, asunto, remitente, destinatario, mensaje.toString(), properties);
+
+        if (enviado) {
+            LOG.info("Mensaje enviado exitosamente");
+        } else {
+            LOG.info("Fallo envio de mail");
+        }
+    }
+}
--- src/main/java/com/fcastillo/jakarta/mail/textoplano/EmailManager.java
+++ src/main/java/com/fcastillo/jakarta/mail/textoplano/EmailManager.java
@@ -3,11 +3,14 @@ package com.fcastillo.jakarta.mail.textoplano;
 import java.util.Properties;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,7 +19,7 @@ import org.apache.logging.log4j.Logger;
  * @author fcastillo
  */
 public class EmailManager {
-    
+
     private static final Logger LOG = LogManager.getLogger(EmailManager.class);
 
     /**
@@ -29,11 +32,11 @@ public class EmailManager {
      * @return true si el mensaje fue enviado, false en caso contrario
      */
     public static boolean enviarTextoPlano(String asunto, String remitente, String destinatario, String mensaje, Properties properties) {
-        
+
         boolean enviado = false;
-        
+
         try {
-            
+
             Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
                 @Override
                 protected PasswordAuthentication getPasswordAuthentication() {
@@ -43,23 +46,23 @@ public class EmailManager {
                     );
                 }
             });
-            
+
             Message message = new MimeMessage(session);
-            
+
             message.setFrom(new InternetAddress(remitente));
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(destinatario));
             message.setSubject(asunto);
             message.setText(mensaje);
-            
+
             Transport.send(message);
-            
+
             enviado = true;
-            
+
         } catch (MessagingException e) {
-            
+
             LOG.error("Ocurrió un error y no pudo enviarse el mail." + e);
         }
-        
+
         return enviado;
     }
 
@@ -75,11 +78,11 @@ public class EmailManager {
      * @return true si el mensaje fue enviado, caso contrario false
      */
     public static boolean enviarConCopia(String[] to, String[] cc, String[] bcc, String asunto, String remitente, String mensaje, Properties properties) {
-        
+
         boolean enviado = false;
-        
+
         try {
-            
+
             Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
                 @Override
                 protected PasswordAuthentication getPasswordAuthentication() {
@@ -89,14 +92,14 @@ public class EmailManager {
                     );
                 }
             });
-            
+
             Message message = new MimeMessage(session);
 
             // Destinatarios principales
             if (to != null) {
-                
+
                 InternetAddress[] addressTO = new InternetAddress[to.length];
-                
+
                 for (int i = 0; i < to.length; i++) {
                     addressTO[i] = new InternetAddress(to[i]);
                     message.addRecipient(Message.RecipientType.TO, addressTO[i]);
@@ -105,9 +108,9 @@ public class EmailManager {
 
             // Copias publicas
             if (cc != null) {
-                
+
                 InternetAddress[] addressCC = new InternetAddress[cc.length];
-                
+
                 for (int i = 0; i < cc.length; i++) {
                     addressCC[i] = new InternetAddress(cc[i]);
                     message.addRecipient(Message.RecipientType.CC, addressCC[i]);
@@ -116,28 +119,98 @@ public class EmailManager {
 
             // Copias privadas
             if (bcc != null) {
-                
+
                 InternetAddress[] addressBCC = new InternetAddress[bcc.length];
-                
+
                 for (int i = 0; i < bcc.length; i++) {
                     addressBCC[i] = new InternetAddress(bcc[i]);
                     message.addRecipient(Message.RecipientType.BCC, addressBCC[i]);
                 }
             }
-            
+
             message.setFrom(new InternetAddress(remitente));
             message.setSubject(asunto);
             message.setText(mensaje);
-            
+
             Transport.send(message);
-            
+
             enviado = true;
-            
+
         } catch (MessagingException e) {
-            
+
             LOG.error("Ocurrió un error y no se pudo enviar el mail " + e);
-            
+
         }
         return enviado;
     }
+
+    /**
+     *
+     * @param archivos archivos a ser enviados como adjuntos
+     * @param asunto asunto del mensaje
+     * @param remitente direccion de correo del remitente
+     * @param destinatario direccion de correo del destinatario
+     * @param mensaje cuerpo del mensaje
+     * @param properties configuraciones
+     * @return true si el mensaje fue enviado, false en caso contrario
+     */
+    public static boolean enviarAdjuntos(String[] archivos, String asunto, String remitente, String destinatario, String mensaje, Properties properties) {
+
+        boolean enviado = false;
+
+        try {
+            Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(
+                            properties.getProperty("mail.smtp.user"),
+                            properties.getProperty("mail.smtp.password")
+                    );
+                }
+            });
+
+            // Compones la parte de texto
+            MimeBodyPart bodyPartTexto = new MimeBodyPart();
+
+            bodyPartTexto.setText(mensaje);
+
+            Multipart multipart = new MimeMultipart();
+
+            if (archivos != null) {
+
+                for (String archivo : archivos) {
+                    
+                    // Componemos la parte de archivos
+                    MimeBodyPart bodyPartArchivos = new MimeBodyPart();
+
+                    bodyPartArchivos.attachFile(archivo);
+
+                    multipart.addBodyPart(bodyPartArchivos);
+                }
+
+            }
+
+            multipart.addBodyPart(bodyPartTexto);
+
+            Message message = new MimeMessage(session);
+
+            message.setFrom(new InternetAddress(remitente));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(destinatario));
+            message.setSubject(asunto);
+            message.setContent(multipart);
+
+            Transport.send(message);
+
+            enviado = true;
+
+        } catch (Exception e) {
+
+            LOG.error("Ocurrió un error y no pudo enviarse el mensaje " + e);
+
+        }
+
+        return enviado;
+
+    }
+
 }
